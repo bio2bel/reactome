054212b3421f9cb4f1b92daafd79fbb509f3a6b9
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2017-09-30 12:06:45 +0200

    uniprot and chebi models added

--- src/bio2bel_reactome/models.py
+++ src/bio2bel_reactome/models.py
@@ -10,18 +10,34 @@ from pybel.constants import FUNCTION, NAMESPACE, NAME, BIOPROCESS
 
 Base = declarative_base()
 
-TABLE_PREFIX = "reactome"
-PATHWAY_TABLE_NAME = "{}_pathway".format(TABLE_PREFIX)
-PATHWAY_TABLE_HIERARCHY = "{}_pathway_hierarchy".format(TABLE_PREFIX)
-SPECIES_TABLE_NAME = "{}_species".format(TABLE_PREFIX)
-UNIPROT_TABLE_NAME = "{}_uniprot".format(TABLE_PREFIX)
-CHEBI_TABLE_NAME = "{}_chebi".format(TABLE_PREFIX)
+TABLE_PREFIX = 'reactome'
+PATHWAY_TABLE_NAME = '{}_pathway'.format(TABLE_PREFIX)
+PATHWAY_TABLE_HIERARCHY = '{}_pathway_hierarchy'.format(TABLE_PREFIX)
+SPECIES_TABLE_NAME = '{}_species'.format(TABLE_PREFIX)
+UNIPROT_TABLE_NAME = '{}_uniprot'.format(TABLE_PREFIX)
+CHEBI_TABLE_NAME = '{}_chebi'.format(TABLE_PREFIX)
+UNIPROT_PATHWAY_TABLE = '{}_uniprot_pathway'.format(TABLE_PREFIX)
+CHEBI_PATHWAY_TABLE = '{}_chebi_pathway'.format(TABLE_PREFIX)
 
 pathway_hierarchy = Table(
     PATHWAY_TABLE_HIERARCHY,
     Base.metadata,
-    Column("parent_id", Integer, ForeignKey("{}.id".format(PATHWAY_TABLE_NAME)), primary_key=True),
-    Column("child_id", Integer, ForeignKey("{}.id".format(PATHWAY_TABLE_NAME)), primary_key=True)
+    Column('parent_id', Integer, ForeignKey('{}.id'.format(PATHWAY_TABLE_NAME)), primary_key=True),
+    Column('child_id', Integer, ForeignKey('{}.id'.format(PATHWAY_TABLE_NAME)), primary_key=True)
+)
+
+uniprot_pathway = Table(
+    UNIPROT_PATHWAY_TABLE,
+    Base.metadata,
+    Column('uniprot_id', Integer, ForeignKey(UNIPROT_TABLE_NAME + 'uniprot_id')),
+    Column('pathway_id', Integer, ForeignKey(PATHWAY_TABLE_NAME + 'reactome_id'))
+)
+
+chebi_pathway = Table(
+    CHEBI_PATHWAY_TABLE,
+    Base.metadata,
+    Column('chebi_id', Integer, ForeignKey(CHEBI_TABLE_NAME + 'chebi_id')),
+    Column('pathway_id', Integer, ForeignKey(PATHWAY_TABLE_NAME + 'reactome_id'))
 )
 
 
@@ -30,23 +46,34 @@ class Pathway(Base):
 
     __tablename__ = PATHWAY_TABLE_NAME
 
-    reactome_id = Column(Integer, primary_key=True)
+    reactome_id = Column(String(255), primary_key=True)
 
     pathway_name = Column(String(255))
     pathway_species = Column(String(255))
 
     children = relationship(
-        "Pathway",
+        'Pathway',
         secondary=pathway_hierarchy,
         primaryjoin=(id == pathway_hierarchy.c.parent_id),
         secondaryjoin=(id == pathway_hierarchy.c.child_id)
     )
 
-    species = relationship("Species", back_populates="pathways")
+    species = relationship('Species', back_populates='pathways')
+
+    genes = relationship(
+        'UniProt',
+        secondary=uniprot_pathway,
+        backref='pathways'
+    )
+
+    chemicals = relationship(
+        'Chebi',
+        secondary=chebi_pathway,
+        backref='pathways'
+    )
 
     def __repr__(self):
         return self.pathway_name
-
 
     def serialize_to_pathway_node(self):
         """Function to serialize to PyBEL node data dictionary.
@@ -70,3 +97,25 @@ class Species(Base):
 
     def __repr__(self):
         return self.species_name
+
+
+class UniProt(Base):
+    """Genes Table"""
+
+    __tablename__ = UNIPROT_TABLE_NAME
+
+    uniprot_id = Column(String(255), primary_key=True)
+
+    def __repr__(self):
+        return self.uniprot_id
+
+
+class Chebi(Base):
+    """Chemical Chebi Table"""
+
+    __tablename__ = CHEBI_TABLE_NAME
+
+    chebi_id = Column(Integer, primary_key=True)
+
+    def __repr__(self):
+        return self.chebi_id
