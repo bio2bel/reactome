e073d7ba2b964df84357f29c83f5f83203e60d22
ddomingofernandez <danieldomingofernandez@hotmail.com>
2017-11-06 16:29:41 +0100

    added more tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -4,17 +4,48 @@
 
 import unittest
 
-from bio2bel_reactome.models import Pathway
+from bio2bel_reactome.models import Pathway, Species, Chemical, Protein
 from tests.constants import DatabaseMixin
 
 
 class TestParse(DatabaseMixin):
     """Tests the parsing module"""
 
-    def test1(self):
+    def test_pathway_count(self):
         pathway_number = self.manager.session.query(Pathway).count()
+        self.assertEqual(21, pathway_number)
 
-        self.assertEquals(pathway_number, 21)
+    def test_chemical_count(self):
+        chemical_number = self.manager.session.query(Chemical).count()
+        self.assertEqual(4, chemical_number)
+
+    def test_protein_count(self):
+        protein_number = self.manager.session.query(Protein).count()
+        self.assertEqual(3, protein_number)
+
+    def test_species_count(self):
+        species_number = self.manager.session.query(Species).count()
+        self.assertEqual(18, species_number)
+
+    def test_empty_pathway_chemicals(self):
+        pathway = self.manager.get_pathway_by_id('R-DME-389357')
+        self.assertIsNotNone(pathway)
+        self.assertEqual(0, len(pathway.chemicals))
+
+    def test_empty_pathway_proteins(self):
+        pathway = self.manager.get_pathway_by_id('R-DME-389357')
+        self.assertIsNotNone(pathway)
+        self.assertEqual(0, len(pathway.proteins))
+
+    def test_drugs(self):
+        pathway = self.manager.get_pathway_by_id('R-HSA-389357')
+        self.assertIsNotNone(pathway)
+        self.assertEqual(4, len(pathway.chemicals))
+
+    def test_drugs_to_pathways(self):
+        chemical = self.manager.session.query(Chemical).filter(Chemical.chebi_id == '16761').one_or_none()
+        self.assertIsNotNone(chemical)
+        self.assertEqual(1, len(chemical.pathways))
 
 
 if __name__ == '__main__':
