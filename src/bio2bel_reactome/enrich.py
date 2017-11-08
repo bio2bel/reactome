df1715d8ca08a70368f4e7368433bab52bf11f1c
ddomingofernandez <danieldomingofernandez@hotmail.com>
2017-11-08 08:36:54 +0100

    added first enrichment tests

--- src/bio2bel_reactome/enrich.py
+++ src/bio2bel_reactome/enrich.py
@@ -1,1 +1,18 @@
-""""""
+# -*- coding: utf-8 -*-
+
+""" Enrich functions for BEL graphs"""
+
+__all__ = [
+    'map_hgnc_node'
+]
+
+
+def map_hgnc_node(manager, identifier):
+    """Maps a gene from the PyHGNC database, whether it has a HGNC, RGD, MGI, or EG identifier.
+
+    :param pyhgnc.manager.query.QueryManager manager: A PyHGNC database manager
+    :param pybel.BELGraph graph: A BEL graph
+    :rtype: pyhgnc.manager.models.HGNC
+    """
+
+    raise NotImplementedError
--- tests/test_enrich.py
+++ tests/test_enrich.py
@@ -1,3 +1,29 @@
 # -*- coding: utf-8 -*-
 
 """ This module contains the tests related with the graph enrichment"""
+
+from bio2bel_reactome.enrich import map_hgnc_node
+from bio2bel_reactome.models import Protein
+from tests.constants import DatabaseMixin
+
+
+class TestParse(DatabaseMixin):
+    """Tests the parsing module"""
+
+    def help_check_rat_cd80_model(self, model):
+        """Checks if the given model is CD33 (https://rgd.mcw.edu/rgdweb/report/gene/main.html?id=RGD:2314)
+        :param pyhgnc.manager.models.HGNC model: The result from a search of the PyHGNC database
+        """
+        self.assertEqual('2314', str(model.identifier))
+        self.assertEqual('Cd80', model.symbol)
+        self.assertEqual('Cd80 molecule', model.name)
+
+    def test_uniprot_rgd_mapping(self):
+        """ Maps uniprot id http://www.uniprot.org/uniprot/A0A0G2K0F2 to Rat Cd80"""
+
+        pyhgnc_manager = None
+        cd80_uniprot = self.manager.session.query(Protein).filter(Protein.uniprot_id == 'A0A0G2K0F2').one_or_none()
+
+        cd80_hgnc = map_hgnc_node(manager=pyhgnc_manager, identifier=cd80_uniprot)
+
+        self.help_check_rat_cd80_model(cd80_hgnc)
