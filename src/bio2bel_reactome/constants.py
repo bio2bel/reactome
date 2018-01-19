52d77969514917f3d976e8933665ba800869cf62
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2018-01-19 20:46:49 +0100

    https protocol in constants

--- src/bio2bel_reactome/constants.py
+++ src/bio2bel_reactome/constants.py
@@ -8,15 +8,16 @@ MODULE_NAME = 'reactome'
 DATA_DIR = get_data_dir(MODULE_NAME)
 DEFAULT_CACHE_CONNECTION = get_connection(MODULE_NAME)
 
-PATHWAY_NAMES_URL = 'http://reactome.org/download/current/ReactomePathways.txt'
-PATHWAYS_HIERARCHY_URL = 'http://reactome.org/download/current/ReactomePathwaysRelation.txt'
-UNIPROT_PATHWAYS_URL = 'http://reactome.org/download/current/UniProt2Reactome_All_Levels.txt'
-CHEBI_PATHWAYS_URL = 'http://reactome.org/download/current/ChEBI2Reactome_All_Levels.txt'
+PATHWAY_NAMES_URL = 'https://reactome.org/download/current/ReactomePathways.txt'
+PATHWAYS_HIERARCHY_URL = 'https://reactome.org/download/current/ReactomePathwaysRelation.txt'
+UNIPROT_PATHWAYS_URL = 'https://reactome.org/download/current/UniProt2Reactome_All_Levels.txt'
+CHEBI_PATHWAYS_URL = 'https://reactome.org/download/current/ChEBI2Reactome_All_Levels.txt'
 
-REACTOME_PATHWAY_LINK = 'http://reactome.org/PathwayBrowser/'
+REACTOME_PATHWAY_LINK = 'https://reactome.org/PathwayBrowser/'
 
 # Namespace constants
 
 REACTOME = 'REACTOME'
 HGNC = 'HGNC'
+UNIPROT = 'UNIPROT'
 CHEBI = 'CHEBI'
