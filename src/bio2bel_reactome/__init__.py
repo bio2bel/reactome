ec159139a7daee21193819767bacc9b59d048925
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-02-14 15:39:31 +0100

    Add bumpversion config

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -11,7 +11,7 @@ __title__ = 'bio2bel_reactome'
 __description__ = "A package for converting Reactome to BEL"
 __url__ = 'https://github.com/bio2bel/reactome'
 
-__author__ = 'Charles Tapley Hoyt and Daniel Domingo-Fernández'
+__author__ = 'Daniel Domingo-Fernández and Charles Tapley Hoyt'
 __email__ = 'charles.hoyt@scai.fraunhofer.de'
 
 __license__ = 'MIT License'
