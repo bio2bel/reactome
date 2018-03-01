d6314dfa1ff15ce7a3ea671867334d7fde2adbfd
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-03-01 16:43:34 +0100

    Fix those spaces in lists!

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""
-Bio2BEL Reactome is a package for enriching BEL networks with Reactome information by wrapping its RESTful API.
+"""Bio2BEL Reactome is a package for enriching BEL networks with Reactome information by wrapping its RESTful API.
+
 Reactome is a pathway database comprising established pathways between different species that contain genetic as well
 as chemical information. This package downloads pathway information from Reactome's API and store it in template
 data model relating genes and chemical to pathways. Moreover, the hierarchy of the pathways is maintained enabling
@@ -10,10 +10,9 @@ pathway comparison and exploration in the `ComPath environment <https://github.c
 Citation
 --------
 - Fabregat, Antonio et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 44.Database issue (2016):
-D481–D487. PMC. Web. 6 Oct. 2017.
-
+  D481–D487. PMC. Web. 6 Oct. 2017.
 - Croft, David et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 42.Database issue (2014): D472–D477.
- PMC. Web. 6 Oct. 2017.
+  PMC. Web. 6 Oct. 2017.
 
 """
 
