06dc09a8c8d54ff839f05860ef50568b114b7575
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2018-03-01 16:34:26 +0100

    Fix linkk documentation

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -5,7 +5,7 @@ Bio2BEL Reactome is a package for enriching BEL networks with Reactome informati
 Reactome is a pathway database comprising established pathways between different species that contain genetic as well
 as chemical information. This package downloads pathway information from Reactome's API and store it in template
 data model relating genes and chemical to pathways. Moreover, the hierarchy of the pathways is maintained enabling
-pathway comparison and exploration in the `ComPath environment <https://github.com/ComPath>.
+pathway comparison and exploration in the `ComPath environment <https://github.com/ComPath>`_.
 
 Citation
 --------
