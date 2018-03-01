758cecb92fd856353b73901caff308c60d375538
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2018-03-01 16:30:01 +0100

    Fix indentation problem

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -2,15 +2,18 @@
 
 """
 Bio2BEL Reactome is a package for enriching BEL networks with Reactome information by wrapping its RESTful API.
-Reactome is a pathway database comprising established pathways between different species that contain genetic as well as chemical information.
-This package downloads pathway information from Reactome's API and store it in template data model relating genes and chemical to pathways.
-Moreover, the hierarchy of the pathways is maintained enabling pathway comparison and exploration in the
- `ComPath environment <https://github.com/ComPath>.
+Reactome is a pathway database comprising established pathways between different species that contain genetic as well
+as chemical information. This package downloads pathway information from Reactome's API and store it in template
+data model relating genes and chemical to pathways. Moreover, the hierarchy of the pathways is maintained enabling
+pathway comparison and exploration in the `ComPath environment <https://github.com/ComPath>.
 
 Citation
 --------
-- Fabregat, Antonio et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 44.Database issue (2016): D481–D487. PMC. Web. 6 Oct. 2017.
-- Croft, David et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 42.Database issue (2014): D472–D477. PMC. Web. 6 Oct. 2017.
+- Fabregat, Antonio et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 44.Database issue (2016):
+D481–D487. PMC. Web. 6 Oct. 2017.
+
+- Croft, David et al. The Reactome Pathway Knowledgebase. Nucleic Acids Research 42.Database issue (2014): D472–D477.
+ PMC. Web. 6 Oct. 2017.
 
 """
 
