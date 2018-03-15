57077c6a0debf4e35512d5466b2b7482a9804020
daniel.domingo.fernandez <danieldomingofernandez@hotmail.com>
2018-03-15 18:09:50 +0100

    Ensure get gene set returns hgnc symbols

--- src/bio2bel_reactome/models.py
+++ src/bio2bel_reactome/models.py
@@ -87,7 +87,7 @@ class Pathway(Base):
         :rtype: set[bio2bel_reactome.models.Protein]
         """
         return {
-            protein
+            protein.hgnc_symbol
             for protein in self.proteins
             if protein.hgnc_symbol
         }
