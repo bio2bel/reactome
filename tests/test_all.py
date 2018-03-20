183e74675f556f0440ccc2a9ac9dca64029c2f20
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-20 11:29:35 +0100

    Update test gene set info

--- tests/test_all.py
+++ tests/test_all.py
@@ -187,7 +187,15 @@ class TestGlobal(DatabaseMixin):
                 "pathway_name": "CD28 dependent Vav1 pathway",
                 "mapped_proteins": 1,
                 "pathway_size": 7,
-                "pathway_gene_set": {}
+                "pathway_gene_set": {
+                    'HGNC_SYMBOL_1',
+                    'HGNC_SYMBOL_2',
+                    'HGNC_SYMBOL_3',
+                    'HGNC_SYMBOL_4',
+                    'HGNC_SYMBOL_5',
+                    'HGNC_SYMBOL_6',
+                    'PFKM'
+                }
             },
             enriched_pathways["R-HSA-389359"]
         )
@@ -198,7 +206,7 @@ class TestGlobal(DatabaseMixin):
                 "pathway_name": "CD28 dependent PI3K/Akt signaling",
                 "mapped_proteins": 1,
                 "pathway_size": 3,
-                "pathway_gene_set": {}
+                "pathway_gene_set": {'HGNC_SYMBOL_3', 'PFKM', 'HGNC_SYMBOL_2'}
             },
             enriched_pathways["R-RNO-389357"]
         )
@@ -209,7 +217,7 @@ class TestGlobal(DatabaseMixin):
                 "pathway_name": "CD28 co-stimulation",
                 "mapped_proteins": 1,
                 "pathway_size": 3,
-                "pathway_gene_set": {}
+                "pathway_gene_set": {'PFKM', 'HGNC_SYMBOL_3', 'HGNC_SYMBOL_1'}
             },
             enriched_pathways["R-HSA-389356"]
         )
@@ -236,7 +244,7 @@ class TestGlobal(DatabaseMixin):
                 "pathway_name": "CD28 co-stimulation",
                 "mapped_proteins": 1,
                 "pathway_size": 3,
-                "pathway_gene_set": {}
+                "pathway_gene_set": {'PFKM', 'HGNC_SYMBOL_3', 'HGNC_SYMBOL_1'}
             },
             enriched_pathways["R-HSA-389356"]
         )
@@ -282,7 +290,7 @@ class TestGlobal(DatabaseMixin):
                 "pathway_name": "CD28 co-stimulation",
                 "mapped_proteins": 2,
                 "pathway_size": 3,
-                "pathway_gene_set": {}
+                "pathway_gene_set": {'PFKM', 'HGNC_SYMBOL_3', 'HGNC_SYMBOL_1'}
             },
             enriched_pathways["R-HSA-389356"]
         )
