d5456dfa28c691dab26cc0afbf288f71990b7581
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-23 15:45:19 +0100

    Bump version: 0.0.3-dev → 0.0.3

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -18,7 +18,7 @@ Citation
 
 from .manager import Manager
 
-__version__ = '0.0.3-dev'
+__version__ = '0.0.3'
 
 __title__ = 'bio2bel_reactome'
 __description__ = "A wrapper around Reactome RESTful API"
