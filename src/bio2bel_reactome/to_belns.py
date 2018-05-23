0472d9a642a66272cfd9d96b07e97f4c6cfcc71a
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-05-23 10:47:50 -0400

    Remove artifactory deployment code

--- src/bio2bel_reactome/to_belns.py
+++ src/bio2bel_reactome/to_belns.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 import logging
 
 from bio2bel_reactome.constants import PATHWAY_NAMES_URL
 from bio2bel_reactome.parsers.pathway_names import get_pathway_names_df
 from pybel.constants import NAMESPACE_DOMAIN_BIOPROCESS
-from pybel.resources.arty import get_today_arty_namespace
 from pybel.resources.definitions import write_namespace
-from pybel.resources.deploy import deploy_namespace
-
 
 log = logging.getLogger(__name__)
 
@@ -51,18 +46,3 @@ def write_belns(file=None, url=None):
         author_contact="charles.hoyt@scai.fraunhofer.de",
         file=file
     )
-
-
-def deploy_to_arty(quit_fail_redeploy=True):
-    """Get the data, writes BEL namespace, and writes BEL knowledge to Artifactory."""
-
-    file_name = get_today_arty_namespace(MODULE_NAME)
-
-    with open(file_name, 'w') as file:
-        write_belns(file)
-
-    namespace_deploy_success = deploy_namespace(file_name, MODULE_NAME)
-
-    if not namespace_deploy_success and quit_fail_redeploy:
-        log.warning('did not redeploy')
-        return False
