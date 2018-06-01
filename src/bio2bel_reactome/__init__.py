bc6e4817eafbfa2efd0f29445a8ea180db06243c
Charles Hoyt <cthoyt@microvac.speedport.ip>
2018-06-01 19:27:56 +0200

    Patch version [skip ci]

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -18,7 +18,7 @@ Citation
 
 from .manager import Manager
 
-__version__ = '0.0.9'
+__version__ = '0.0.10-dev'
 
 __title__ = 'bio2bel_reactome'
 __description__ = "A wrapper around Reactome RESTful API"
