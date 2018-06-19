4ccb2d4ac02cad7fa7dc7c8b8fbef4f9a74cb9dd
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-06-19 14:36:43 +0200

    Revert previous commit

--- src/bio2bel_reactome/web.py
+++ src/bio2bel_reactome/web.py
@@ -7,6 +7,4 @@ from .manager import Manager
 if __name__ == '__main__':
     manager = Manager()
     app = manager.get_flask_admin_app()
-    app.secret_key = 'a\x1c\xd4\x1b\xb1\x05\xac\xac\xee\xcb6\xd8\x9fl\x14%B\xd2W\x9fP\x06\xcb\xff'
-    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True, host='0.0.0.0', port=5000)
