b10c9fe2424b274f0f1eab0555e800d4208134c9
Charles Tapley Hoyt <cthoyt@wlan-224.scai.fraunhofer.de>
2018-08-13 17:44:55 +0200

    Bump version: 0.0.12 → 0.1.0-dev

--- src/bio2bel_reactome/__init__.py
+++ src/bio2bel_reactome/__init__.py
@@ -18,7 +18,7 @@ Citation
 
 from .manager import Manager
 
-__version__ = '0.0.12'
+__version__ = '0.1.0-dev'
 
 __title__ = 'bio2bel_reactome'
 __description__ = "A wrapper around Reactome RESTful API"
