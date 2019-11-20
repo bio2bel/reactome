6ccbdf3b3176a22719d7fd17b326a67b831d332d
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2019-11-20 14:48:14 +0100

    Upgrate to pybel 0.14

--- src/bio2bel_reactome/to_belns.py
+++ src/bio2bel_reactome/to_belns.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from typing import Dict
+
+from bel_resources import write_namespace
 
 from bio2bel_reactome.constants import PATHWAY_NAMES_URL
 from bio2bel_reactome.parsers.pathway_names import get_pathway_names_df
 from pybel.constants import NAMESPACE_DOMAIN_BIOPROCESS
-from bel_resources import write_namespace
 
 log = logging.getLogger(__name__)
 
 MODULE_NAME = 'reactome'
 
 
-def get_values(url=None):
+def get_values(url=None) -> Dict[str, str]:
     """Get the unique names from Reactome pathway names table. Combines all species.
 
     :param Optional[str] url: A non-default URL for the Reactome pathway names table
-    :rtype: set[str]
     """
     df = get_pathway_names_df(url=url)
 
     values = set(df[1])
 
-    return values
+    return {
+        value: "B"
+        for value in values
+    }
 
 
 def write_belns(file=None, url=None):
@@ -33,16 +37,15 @@ def write_belns(file=None, url=None):
     values = get_values(url=url)
 
     write_namespace(
+        values=values,
         namespace_name="Reactome Pathway Names",
         namespace_keyword="REACTOME",
         namespace_domain=NAMESPACE_DOMAIN_BIOPROCESS,
         author_name='Charles Tapley Hoyt',
         citation_name=PATHWAY_NAMES_URL,
-        values=values,
         namespace_species='9606',
         namespace_description="Reactome Pathways",
         author_copyright='Creative Commons by 4.0',
-        functions="B",
         author_contact="charles.hoyt@scai.fraunhofer.de",
         file=file
     )
